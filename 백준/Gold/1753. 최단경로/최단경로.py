d93b9a9a2cde01366adefc311506857f78c08fab
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-03-12 17:53:02 +0900

    [Gold IV] Title: 최단경로, Time: 628 ms, Memory: 68528 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,50 +1,35 @@
 import sys
+import heapq
 input = sys.stdin.readline
+
+n,m = map(int,input().split())
+start = int(input())
 INF = int(1e9)
-
-n,m = map(int,input().split())  # 노드의 개수, 간선의 개수
-start = int(input())    # 시작 노드
-graph = [[] for _ in range(n+1)]    # 인접리스트
-visited = [0] * (n+1)   # 방문 테이블
-distance = [INF] * (n+1) # 거리 테이블
-
+graph = [[] for _ in range(n+1)]
+distance = [INF for _ in range(n+1)]
 for _ in range(m):
     a,b,c = map(int,input().split())
     graph[a].append((b,c))
 
-def get_smallest_node():
-    min_value = INF
-    index = 0
-    for i in range(1,n+1):
-        if not visited[i] and distance[i] < min_value:
-            min_value = distance[i]
-            index = i
-    return index
-
-def dijkstra(start): # 단방향이고 모든 가중치가 0 이상일때만 사용 가능
+def dijkstra(start):
+    q = []
+    heapq.heappush(q,(0,start))
     distance[start] = 0
-    visited[start] = 1
-
-    for i in graph[start]:
-        if distance[i[0]] != 0:
-            if i[1] < distance[i[0]]:
-                distance[i[0]] = i[1]
-        else:
-            distance[i[0]] = i[1]  # i[0] : 연결 노드, i[1] : 가중치
-
-    for _ in range(n-1):
-        now = get_smallest_node()   # 방문하지 않고 바로 위에서 구한 연결된 노드들
-        visited[now] = 1
-
-        for after in graph[now] :
-            cost = distance[now] + after[1]
+    while q:
+        dist, node = heapq.heappop(q)
+        if distance[node] < dist :
+            continue
+        for after in graph[node] :
+            cost = distance[node] + after[1]
             if cost < distance[after[0]] :
                 distance[after[0]] = cost
+                heapq.heappush(q,(cost,after[0]))
 
 dijkstra(start)
 
-for i in range(1,n+1):
-    if distance[i] == INF:
+for i in range(1,len(distance)):
+    if distance[i] == INF :
         print('INF')
-    else :
+    else:
         print(distance[i])
+
