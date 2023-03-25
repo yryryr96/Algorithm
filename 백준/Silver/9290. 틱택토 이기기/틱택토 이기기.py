32a97891add5c30eee6ce55c9c628403b469c722
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-03-25 22:30:49 +0900

    [Silver IV] Title: 틱택토 이기기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9290. 틱택토 이기기/틱택토 이기기.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+point = [[0,1],[1,0],[-1,0],[0,-1],[1,1],[1,-1],[-1,1],[-1,-1]]
+
+def search(i,j,t):
+
+    for di,dj in point :
+        ni,nj = i+di, j+dj
+
+        if 0<=ni<3 and 0<=nj<3 and graph[ni][nj] == t :
+            if 0<=ni+di<3 and 0<=nj+dj<3 :
+                graph[ni+di][nj+dj] = t
+                return
+
+        elif 0<=ni+di<3 and 0<=nj+dj<3 and graph[ni+di][nj+dj] == t :
+            if 0<=ni<3 and 0<=nj<3 :
+                graph[ni][nj] = t
+                return
+
+for tc in range(1,n+1):
+    graph = [list(input().rstrip()) for _ in range(3)]
+    team = input().rstrip()
+    for i in range(3):
+        for j in range(3):
+            if graph[i][j] == team :
+                search(i,j,team)
+                
+
+    print(f'Case {tc}:')
+    for i in range(3):
+        print(''.join(graph[i]))
