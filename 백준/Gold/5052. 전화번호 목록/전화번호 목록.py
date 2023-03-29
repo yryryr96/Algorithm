96b1de715da04da6f753bbde82d4fdab383414ef
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-03-29 22:00:12 +0900

    [Gold IV] Title: 전화번호 목록, Time: 164 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -7,15 +7,14 @@ for _ in range(t):
     lst = []
     for _ in range(n):
         lst.append(input().rstrip())
-    lst.sort(key=len)
-    temp = 0
+    lst.sort()
+
     for i in range(len(lst)-1):
-        for j in range(i+1,len(lst)) :
-            if lst[i][0] == lst[j][0] :
-                if lst[j][:len(lst[i])] == lst[i] :
-                    temp = 1
-                    break
-    if temp == 1:
-        print("NO")
+        if lst[i+1].startswith(lst[i]) :
+            print("NO")
+            break
     else:
         print("YES")
+
+
+
