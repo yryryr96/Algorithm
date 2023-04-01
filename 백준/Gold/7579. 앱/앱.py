940f50ce44655489af25ebde008c759069a6af9f
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-04-01 23:22:58 +0900

    [Gold III] Title: 앱, Time: 612 ms, Memory: 48028 KB -BaekjoonHub

---
+++ 백준/Gold/7579. 앱/앱.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+memory = list(map(int,input().split()))
+charge = list(map(int,input().split()))
+lst = [(0,0)]
+for i in range(n):
+    lst.append((memory[i],charge[i]))
+
+graph = [[0]*(sum(charge)+1) for _ in range(n+1)]
+ans = sys.maxsize
+for i in range(1,n+1):
+    M = lst[i][0]
+    C = lst[i][1]
+    for j in range(1,sum(charge)+1):
+        if C > j :
+            graph[i][j] = graph[i-1][j]
+        else :
+            graph[i][j] = max(graph[i-1][j], graph[i-1][j-C] + M )
+
+        if graph[i][j] >= m :
+            ans = min(ans,j)
+
+print(ans)
