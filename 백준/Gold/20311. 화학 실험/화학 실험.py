eb6decfd369770031314a2061100390fba995c4c
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-04-07 14:04:10 +0900

    [Gold V] Title: 화학 실험, Time: 352 ms, Memory: 59260 KB -BaekjoonHub

--- 백준/Gold/20311. 화학 실험/화학 실험.py
+++ 백준/Gold/20311. 화학 실험/화학 실험.py
@@ -1,28 +1,20 @@
-n, k = map(int, input().split())
-c = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
 
-if max(c) > (n+1)//2:
-    print("-1")
-else:
-    # 누적합 배열 생성
-    prefix_sum = [0] * (k+1)
-    for i in range(1, k+1):
-        prefix_sum[i] = prefix_sum[i-1] + c[i-1]
+N, K = map(int, input().split())
+color = list(map(int, input().split()))
 
-    # 해당 색깔을 가진 시약이 있는 시험관 리스트 생성
-    tube_list = [[] for _ in range(k+1)]
-    for i in range(1, k+1):
-        for j in range(prefix_sum[i-1]+1, prefix_sum[i]+1):
-            tube_list[i].append(j)
+if max(color) > (N+1)//2:
+    print(-1)
+    sys.exit()
 
-    # 시험관 배열 구성
-    result = [0] * n
-    idx = 0
-    for i in range(1, k+1):
-        for j in range(len(tube_list[i])):
-            result[idx] = i
-            idx += 2
-            if idx >= n:
-                idx = 1
+tubes = [0] * N
+idx = 0
+for i in sorted(range(K), key=lambda x: -color[x]):
+    for j in range(color[i]):
+        tubes[idx] = i+1
+        idx += 2
+        if idx >= N:
+            idx = 1
 
-    print(" ".join(map(str, result)))
+print(*tubes)
