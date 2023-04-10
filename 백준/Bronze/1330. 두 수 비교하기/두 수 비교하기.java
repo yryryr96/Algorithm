a114f10630d3996ee4a1d6100bdc171d3d18234d
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-04-10 21:20:29 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 112 ms, Memory: 12980 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,23 @@
+import com.sun.corba.se.impl.orb.ParserTable;
+
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b ;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if (a < b){
+            System.out.println("<");
+        }
+        else if (a > b){
+            System.out.println(">");
+        }
+        else {
+            System.out.println("==");
+        }
+
+    }
+}
