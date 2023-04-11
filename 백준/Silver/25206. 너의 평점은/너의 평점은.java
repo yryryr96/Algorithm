ed80bf27acc806a26f3dfae3ef98bf071d5bb9e8
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-04-12 08:54:56 +0900

    [Silver V] Title: 너의 평점은, Time: 80 ms, Memory: 11848 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,43 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input="";
-        float cnt = 0;
-        float ans = 0;
-        for (int i = 0; i < 20 ; i++) {
+        String[] rank = {"A+","A0","B+","B0","C+","C0","D+","D0","F"};
+        double[] score = {4.5,4.0,3.5,3.0,2.5,2.0,1.5,1.0,0,0};
+        double sum = 0 , sum_score = 0 ;
+
+        for (int i = 0; i <20 ; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String subject = st.nextToken();
-            double score = Double.parseDouble(st.nextToken());
-            String sub = st.nextToken();
-            if (sub.equals("A+")){
-                ans+=4.5*score;
-            }else if(sub.equals("A0")){
-                ans+=4.0*score;
-            }else if(sub.equals("B+")){
-                ans+=3.5*score;
-            }else if(sub.equals("B0")){
-                ans+=3.0*score;
-            }else if(sub.equals("C+")){
-                ans+=2.5*score;
-            }else if(sub.equals("C0")){
-                ans+=2.0*score;
-            }else if(sub.equals("D+")){
-                ans+=1.5*score;
-            }else if(sub.equals("D0")){
-                ans+=1.0*score;
-            }else if(sub.equals("F")){
-                ans+=0;
-            }else if(sub.equals("P")){
-                continue;
+            double t = Double.parseDouble(st.nextToken());
+            String scoreTemp = st.nextToken();
+
+            for (int j = 0; j <9 ; j++) {
+                if (scoreTemp.equals(rank[j])) {
+                    sum_score += t;
+                    sum += t*score[j];
+                    break;
+                }
             }
-            cnt += score;
         }
-        System.out.println(String.format("%.6f",ans/cnt));
+        System.out.println(sum/sum_score);
     }
 }
