06f535e124180ace47c895791acd4d8f8c5c9f78
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-04-11 17:47:15 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 72 ms, Memory: 34308 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque , defaultdict
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+dic = defaultdict()
+
+for _ in range(n):
+    a,b = map(int,input().split())
+    dic[a] = b
+
+for _ in range(m):
+    a,b = map(int,input().split())
+    dic[a] = b
+
+visited = [0]*101
+q = deque()
+q.append(1)
+
+while q:
+    now = q.popleft()
+    
+    if now == 100 :
+        print(visited[100])
+
+    if dic.get(now,0) != 0 :
+        visited[dic[now]] = visited[now]
+        now = dic[now]
+
+    for i in [now+1,now+2,now+3,now+4,now+5,now+6] :
+        if i <= 100 and visited[i] == 0 :
+            visited[i] = visited[now] + 1
+            q.append(i)
