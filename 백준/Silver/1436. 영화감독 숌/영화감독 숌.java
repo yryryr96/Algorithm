6dc954cfedde7e5fa7698427cfd8331425f9350e
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-04-12 22:39:02 +0900

    [Silver V] Title: 영화감독 숌, Time: 348 ms, Memory: 158608 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int num = 666;
+        int cnt = 1;
+
+        while(cnt!=n){
+            num++;
+            if (String.valueOf(num).contains("666")){
+                cnt++;
+            }
+        }
+        System.out.println(num);
+    }
+}
