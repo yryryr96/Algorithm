742582ce8be1d5236da590a7acbc95730c9b77c3
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-04-17 23:23:20 +0900

    [Silver I] Title: 안전 영역, Time: 992 ms, Memory: 34240 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,46 @@
+import sys,copy
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(map(int,input().split())) for _ in range(n)]
+point = [[0,1],[1,0],[-1,0],[0,-1]]
+MAX = 0
+for i in range(n):
+    MAX = max(MAX,max(graph[i]))
+
+def bfs(h):
+    temp = copy.deepcopy(graph)
+    visited = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if temp[i][j] <= h :
+                temp[i][j] = 0
+
+    def island(i,j,a):
+        q = deque()
+        q.append((i,j))
+        while q:
+            i,j = q.popleft()
+            for di,dj in point:
+                ni,nj = i+di,j+dj;
+                if 0<=ni<n and 0<=nj<n and temp[ni][nj]!= 0 and visited[ni][nj] == 0:
+                    visited[ni][nj] = 1
+                    q.append((ni,nj))
+                    temp[ni][nj] = a
+    a = 0
+    for i in range(n):
+        for j in range(n):
+            if temp[i][j] != 0 and visited[i][j] == 0 :
+                a+=1
+                visited[i][j] = 1
+                island(i,j,a)
+
+    return a
+
+ans = 0
+for i in range(MAX+1):
+    k = bfs(i)
+    if ans < k :
+        ans = k
+print(ans)
