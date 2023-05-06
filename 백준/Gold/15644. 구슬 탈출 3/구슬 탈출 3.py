e79e7277b7ba2c09c88f79bbb6510c31d06f0cf4
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-05-06 19:43:55 +0900

    [Gold I] Title: 구슬 탈출 3, Time: 264 ms, Memory: 31988 KB -BaekjoonHub

---
+++ 백준/Gold/15644. 구슬 탈출 3/구슬 탈출 3.py
@@ -0,0 +1,119 @@
+import copy
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+graph = [list(input().rstrip()) for _ in range(n)]
+point = [[-1,0],[0,-1],[1,0],[0,1]]
+alpha = ['U','L','D','R']
+ans = sys.maxsize
+
+def dfs(map,d,w,cnt) : # 그래프, 방향, 몇 번
+    global ans
+    if cnt == 11 :
+        return
+
+    temp = copy.deepcopy(map)
+    check = 0
+    if d == 0 or d == 1 : # 상,좌
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 'R' or temp[i][j] == 'B' :
+                    c = temp[i][j]
+                    di,dj = point[d][0],point[d][1]
+                    ni,nj = i+di, j+dj
+                    ki,kj = i,j
+                    if c == 'R' :
+                        while (0<=ni<n and 0<=nj<m) :
+                            if temp[ni][nj] == '#' or temp[ni][nj] == 'B':
+                                temp[i][j] = '.'
+                                temp[ki][kj] = c
+                                break
+
+                            elif temp[ni][nj] == 'O' :
+                                temp[i][j] = '.'
+                                check += 1
+                                break
+                            ki,kj = ni,nj
+                            ni += di
+                            nj += dj
+
+                    else : # 'C'
+                        while (0<=ni<n and 0<=nj<m):
+                            if temp[ni][nj] == '#' or temp[ni][nj] == 'R':
+                                temp[i][j] = '.'
+                                temp[ki][kj] = c
+                                break
+                            elif temp[ni][nj] == 'O' :
+                                temp[i][j] = '.'
+                                check -= 1
+                                break
+                            ki,kj = ni,nj
+                            ni+=di
+                            nj+=dj
+
+    if d == 2 or d == 3:  # 하,우
+        for i in range(n-1,-1,-1):
+            for j in range(m-1,-1,-1):
+                if temp[i][j] == 'R' or temp[i][j] == 'B':
+                    c = temp[i][j]
+                    di, dj = point[d][0], point[d][1]
+                    ni, nj = i + di, j + dj
+                    ki, kj = i, j
+                    if c == 'R':
+                        while (0 <= ni < n and 0 <= nj < m):
+                            if temp[ni][nj] == '#' or temp[ni][nj] == 'B':
+                                temp[i][j] = '.'
+                                temp[ki][kj] = c
+                                break
+
+                            elif temp[ni][nj] == 'O':
+                                temp[i][j] = '.'
+                                check += 1
+                                break
+                            ki, kj = ni, nj
+                            ni += di
+                            nj += dj
+
+                    else:  # 'C'
+                        while (0 <= ni < n and 0 <= nj < m):
+                            if temp[ni][nj] == '#' or temp[ni][nj] == 'R':
+                                temp[i][j] = '.'
+                                temp[ki][kj] = c
+                                break
+                            elif temp[ni][nj] == 'O':
+                                temp[i][j] = '.'
+                                check -= 2
+                                break
+                            ki, kj = ni, nj
+                            ni += di
+                            nj += dj
+
+
+    if check == 1 :
+        ans = min(ans,cnt)
+        ans_w.append(''.join(w))
+        return
+
+    elif check < 0 :
+        return
+
+    else :
+        for i in range(4):
+            if i == d or i == (d+2)%4 :
+                continue
+            w.append(alpha[i])
+            dfs(temp,i,w,cnt+1)
+            w.pop()
+
+
+ans_w = []
+for i in range(4):
+    dfs(graph,i,[alpha[i]],1)
+
+if ans == sys.maxsize :
+    print(-1)
+else :
+    ans_w.sort(key=len)
+    print(ans)
+    print(ans_w[0])
