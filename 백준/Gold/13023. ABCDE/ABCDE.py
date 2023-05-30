474fab5e382ce5bd4937f1c64bac1b8cf07234e7
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-05-30 17:32:25 +0900

    [Gold V] Title: ABCDE, Time: 1528 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+graph = [[] for _ in range(n)]
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(v,cnt):
+    if cnt == 5 :
+        print(1)
+        exit()
+
+    for i in graph[v] :
+        if visited[i] == 0 :
+            visited[i] = 1
+            dfs(i,cnt+1)
+            visited[i] = 0
+
+for i in range(n):
+    visited = [0]*n
+    dfs(i,0)
+print(0)
