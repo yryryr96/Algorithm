7ad8c44afa3d47b3283d8d9535ceb4f454de0047
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-06-03 23:07:59 +0900

    [Gold IV] Title: 전력난, Time: 988 ms, Memory: 217588 KB -BaekjoonHub

--- 백준/Gold/6497. 전력난/전력난.py
+++ 백준/Gold/6497. 전력난/전력난.py
@@ -1,35 +1,40 @@
 import sys
+
 input = sys.stdin.readline
 
+#유니온 파인드 알고리즘을 통해 부모 노드를 통일
 def find(x):
-    if parent[x] != x :
+    if parent[x] != x:
         parent[x] = find(parent[x])
     return parent[x]
 
-def union(a,b):
-    a = find(a)
-    b = find(b)
-    parent[max(a,b)] = min(a,b)
 
-while True :
-    m,n = map(int,input().split())
-    if m == 0 and n == 0 :
+def union(x, y):
+    x, y = find(x), find(y)
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
+
+
+while True:
+    m, n = map(int, input().split())
+    if m == 0 and n == 0: 
         break
-
-    edges = []
     parent = [i for i in range(m)]
-    total_dist = 0
+    result = 0
+    
+    edges = []
     for _ in range(n):
-        a,b,z = map(int,input().split())
-        edges.append((z,a,b))
-        total_dist += z
+        n1, n2, dist = map(int, input().split()) 
+        edges.append((n1, n2, dist))
 
-    edges.sort()
-    cost = 0
-    cnt = 0
-    for dist,a,b in edges :
-        if find(a) != find(b):
-            union(a,b)
-            cost += dist
+    edges.sort(key=lambda x: x[2]) 
 
-    print(total_dist - cost)
+    for edge in edges:
+        u, v, w = edge
+        if find(u) != find(v): #유니온 파인드로 부모 노드가 같은지 보고, 아니라면 통일해줍니다.
+            union(u, v)
+        else: #문제에서 원하는 것이 절약되는 액수이므로, 연결되지 않은 간선의 가중치를 더해줍니다.
+            result += w
+    print(result)
