ce2d384077aa43b455bab44f60b3b7375b3d9f9b
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2023-10-08 22:11:34 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 1932 ms, Memory: 200516 KB, Score: 43 point -BaekjoonHub

--- 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.py
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.py
@@ -1,41 +1,30 @@
-import sys, heapq
+from heapq import heappop, heappush
+import sys
 input = sys.stdin.readline
-
-N, M, A, B, C = map(int,input().split())
-
-graph = [[] for _ in range(N+1)]
+INF = sys.maxsize
+N, M, s, e, money = map(int, input().split())
+s -= 1; e -= 1
+matrix = [[] for _ in range(N)]
 for _ in range(M):
-    a,b,d = map(int,input().split())
-    graph[a].append((b,d))
-    graph[b].append((a,d))
-
-def dijkstra(A,B) :
-    global C
-
-    q = []
-    heapq.heappush(q, (0,0,A)) # 최대, 총, 현재 노드
-    distance = [1e9] * (N+1)
-    distance[A] = 0
-
-    ans = sys.maxsize
-    while q :
-        md, d, node = heapq.heappop(q)
-
-        if node == B :
-            ans = min(md,ans)
-
-        if distance[node] < d : continue
-
-        for v,dist in graph[node] :
-            cost = distance[node] + dist
-            k = max(md,dist)
-            if k < distance[v] and cost <= C:
-                distance[v] = k
-                heapq.heappush(q,(k,cost,v))
-
-    return ans
-
-k = dijkstra(A,B)
-ans = k if k != sys.maxsize else -1
-print(ans)
-
+    a, b, t = map(int, input().split())
+    a -= 1; b -= 1
+    matrix[a].append((t, b))
+    matrix[b].append((t, a))
+visited = [INF] * N
+visited[s] = 0
+start = []
+heappush(start, [0, s, 0])
+result = INF
+while start:
+    x, node, m = heappop(start)
+    if node == e:
+        if result > visited[node]:
+            result = visited[node]
+    if x > visited[node]:
+        continue
+    for k, v in matrix[node]:
+        nx = max(k, x); nm = m + k
+        if visited[v] > nx and nm <= money:
+            visited[v] = nx
+            heappush(start, [nx, v, nm])
+print(result if result != INF else -1)
