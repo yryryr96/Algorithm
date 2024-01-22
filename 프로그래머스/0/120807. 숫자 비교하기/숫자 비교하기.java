371f99d14140307b9e8925ea77083b4d91a1437e
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2024-01-22 22:48:24 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        int answer = -1;
+        
+        if (num1 == num2) {
+            answer = 1;    
+            return answer;
+        }
+        
+        return answer;
+    }
+}
