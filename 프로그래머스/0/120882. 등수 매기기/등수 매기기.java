704f7597a57b31a7254f2616e67fac6737173f4f
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2024-01-30 21:15:32 +0900

    [level 0] Title: 등수 매기기, Time: 0.35 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -5,20 +5,15 @@ class Solution {
         
         int[] answer = new int[scores.length];
         Integer[] avg = new Integer[scores.length];
-        int[] temp = new int[scores.length];
         
         for (int i=0; i<scores.length; i++) {
             avg[i] = (scores[i][0] + scores[i][1]);
         } 
         
-        for (int i=0; i<temp.length; i++) {
-            temp[i] = avg[i];
-        }
-        
         Arrays.sort(avg, Collections.reverseOrder());
         
-        for (int i=0; i<temp.length; i++) {
-            answer[i] = Arrays.asList(avg).indexOf(temp[i]) + 1;
+        for (int i=0; i<scores.length; i++) {
+            answer[i] = Arrays.asList(avg).indexOf(scores[i][0] + scores[i][1]) + 1;
         }
         return answer;
     }
