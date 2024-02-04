170bdf117c2592ddcbe6d3952301c257ddcf7264
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2024-02-05 01:59:34 +0900

    [level 1] Title: 모의고사, Time: 2.72 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -4,34 +4,14 @@ class Solution {
         int[] one = {1, 2, 3, 4, 5};
         int[] two = { 2, 1, 2, 3, 2, 4, 2, 5};
         int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int i = 0, j = 0, k = 0;
-        int num = 0;
         int[] rank = new int[3];
         ArrayList<Integer> answer = new ArrayList<>();
-        while (num < answers.length) {
 
-            int ans = answers[num];
-            if (ans == one[i]) {
-                rank[0]++;
-            }
-
-            if (ans == two[j]) {
-                rank[1]++;
-            }
-
-            if (ans == three[k]) {
-                rank[2]++;
-            }
-
-            i++;
-            j++;
-            k++;
-
-            if (i == one.length) i = 0;
-            if (j == two.length) j = 0;
-            if (k == three.length) k = 0;
-
-            num++;
+        for (int i = 0; i < answers.length; i++) {
+            int ans = answers[i];
+            if (ans == one[i%5]) rank[0]++;
+            if (ans == two[i%8]) rank[1]++;
+            if (ans == three[i%10]) rank[2]++;
         }
 
         int max = Arrays.stream(rank).max().getAsInt();
