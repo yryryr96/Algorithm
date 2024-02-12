f4d8836735648236410db66f48d0f1f8a95fd062
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2024-02-13 01:23:14 +0900

    [level 1] Title: 체육복, Time: 0.33 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int[] total = new int[n+1];
+        
+        Arrays.fill(total, 1);
+        Arrays.sort(lost);
+        
+        for (int i : lost) {
+            total[i] -= 1;
+        }
+
+        for (int i : reserve) {
+            total[i] += 1;
+        }
+
+        for (int i : lost) {
+            if (total[i] == 0) {
+                if (total[i-1] == 2) {
+                    total[i] += 1;
+                    total[i-1] -= 1;
+                }
+
+                else if (i+1 <= n && total[i+1] == 2) {
+                    total[i] += 1;
+                    total[i+1] -= 1;
+                }
+            }
+        }
+
+        for (int i = 1; i < total.length; i++) {
+            if (total[i] >= 1) answer ++;
+        }
+        return answer;
+    }
+}
