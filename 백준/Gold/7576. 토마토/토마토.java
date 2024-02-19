1461f47595aa949773fbc66b8eb22064e7550947
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2024-02-19 18:34:30 +0900

    [Gold V] Title: 토마토, Time: 692 ms, Memory: 123744 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,69 +1,87 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main{
+public class Main {
+
+    static int[] di = {1,-1,0,0};
+    static int[] dj = {0,0,1,-1};
+    static StringTokenizer st;
+    static int[][] visited;
+    static String[][] map;
+    static BufferedReader br;
+    static Queue<Pair> q = new LinkedList<>();
     static int n;
     static int m;
-    static int[][] graph;
-    static int[][] visited;
-    static Deque<int[]> q;
     static int ans;
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-        graph = new int[n][m];
-        visited = new int[n][m];
 
-        q = new ArrayDeque<>();
-        for (int i = 0; i <n ; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j <m ; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-                if (graph[i][j] == 1){
-                    q.add(new int[] {i,j});
-                    visited[i][j] = 1;
-                }
-
-            }
-        }
-
+        init();
         bfs();
-        ans = 0;
-        int temp = 1;
-        for (int i = 0; i <n ; i++) {
-            for (int j = 0; j <m ; j++) {
-                if (ans < visited[i][j]) ans = visited[i][j];
-                if (graph[i][j] == 0 ) temp = 0;
-            }
-        }
-
-        if (temp == 0 ) System.out.println(-1);
-        else System.out.println(ans-1);
-
+        System.out.println(valid());
     }
 
-    public static void bfs(){
+    public static int valid() {
 
-        int[] di = new int[] {1,-1,0,0};
-        int[] dj = new int[] {0,0,1,-1};
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j].equals("-1")) continue;
+                if (visited[i][j] == -1) return -1;
+                ans = Math.max(ans, visited[i][j]);
+            }
+        }
 
-        while (!q.isEmpty()){
+        return ans;
+    }
 
-            int[] now = q.removeFirst();
-            int i = now[0];
-            int j = now[1];
-            for (int k = 0; k <4 ; k++) {
-                int ni = i+di[k];
-                int nj = j+dj[k];
-                if (0<=ni && n>ni && 0<=nj && m>nj && visited[ni][nj] == 0) {
-                    if (graph[ni][nj] == 0){
-                        visited[ni][nj] = visited[i][j] + 1;
-                        graph[ni][nj] = 1;
-                        q.add(new int[] {ni,nj});
-                    }
+    public static class Pair {
+        int y,x;
+
+        Pair(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+
+    public static void init() throws IOException {
+
+        map = new String[n][m];
+        visited = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = st.nextToken();
+                if (map[i][j].equals("1")) {
+                    visited[i][j] = 0;
+                    q.add(new Pair(i,j));
+                } else {
+                    visited[i][j] = -1;
+                }
+            }
+        }
+    }
+
+    public static void bfs() {
+
+        while (!q.isEmpty()) {
+
+            Pair p = q.poll();
+            int i = p.y;
+            int j = p.x;
+
+            for (int k = 0; k < 4; k++) {
+                int ni = i + di[k]; int nj = j + dj[k];
+                if (0 > ni || n <= ni || 0 > nj || m <= nj || visited[ni][nj] != -1) continue;
+                if (map[ni][nj].equals("0")) {
+                    visited[ni][nj] = visited[i][j] + 1;
+                    q.add(new Pair(ni,nj));
                 }
             }
         }
