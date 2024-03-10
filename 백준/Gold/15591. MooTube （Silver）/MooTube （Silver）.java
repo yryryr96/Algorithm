c2365ce49f5bd6168b7d6ce4cc02c6a1a5db79f9
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2024-03-10 22:50:34 +0900

    [Gold V] Title: MooTube (Silver), Time: 1504 ms, Memory: 300492 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.java
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.java
@@ -30,22 +30,25 @@ public class Main {
 
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < Q; i++) {
             st = new StringTokenizer(br.readLine());
             int k = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            System.out.println(bfs(k,v));
+            sb.append(bfs(k,v)).append("\n");
         }
+
+        System.out.println(sb.toString());
     }
 
     static int bfs(int k, int v) {
 
-        long[] distance = new long[N+1];
-        Arrays.fill(distance, INF);
         boolean[] visited = new boolean[N+1];
         Queue<Integer> q = new LinkedList<>();
 
         q.add(v);
+        int cnt = 0;
+
         while(!q.isEmpty()) {
 
             int node = q.poll();
@@ -54,17 +57,12 @@ public class Main {
                 int dist = info[1];
 
                 if (next == v) continue;
-                if (visited[next] == false && Math.min(dist, distance[node]) < distance[next]) {
+                if (!visited[next] && k <= dist) {
                     visited[next] = true;
-                    distance[next] = Math.min(dist, distance[node]);
                     q.add(next);
+                    cnt++;
                 }
             }
-        }
-
-        int cnt = 0;
-        for (int i = 1; i < N+1; i++) {
-            if (i != v && distance[i] >= k && distance[i] != INF) cnt++;
         }
 
         return cnt;
