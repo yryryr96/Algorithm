9a03a38428f882125f860a9a96a4c1d8ee95b215
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2024-03-18 03:38:52 +0900

    [Gold V] Title: 회장뽑기, Time: 120 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Gold/2660. 회장뽑기/회장뽑기.java
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.java
@@ -3,11 +3,11 @@ import java.util.*;
 
 public class Main {
 
-    static int INF = 987654321;
     static int n;
     static int[][] relation;
-    static int candidateScore = INF;
+    static int candidateScore = Integer.MAX_VALUE;
     static int candidateCount = 0;
+    static List<Integer> candidate = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
@@ -16,12 +16,6 @@ public class Main {
 
         n = Integer.parseInt(br.readLine());
         relation = new int[n+1][n+1];
-
-        for (int i = 1; i < n+1; i++) {
-            for (int j = 1; j < n+1; j++) {
-                if (i != j) relation[i][j] = INF;
-            }
-        }
 
         while (true) {
 
@@ -38,41 +32,43 @@ public class Main {
         for (int k = 1; k < n+1; k++) {
             for (int i = 1; i < n+1; i++) {
                 for (int j = 1; j < n+1; j++) {
-                    if (relation[i][j] > relation[i][k] + relation[k][j]) {
-                        relation[i][j] = relation[i][k] + relation[k][j];
+                    int cnt = 0;
+                    if (i == j) continue;
+                    if (relation[i][k] != 0 && relation[k][j] != 0) {
+                        int dist = relation[i][k] + relation[k][j];
+
+                        if (relation[i][j] == 0) relation[i][j] = dist;
+                        else relation[i][j] = Math.min(dist, relation[i][j]);
                     }
                 }
             }
         }
 
-        int[] scoreArr = new int[n+1];
-
         for (int i = 1; i < n+1; i++) {
-
             int sum = 0;
             for (int j = 1; j < n+1; j++) {
-                if (relation[i][j] != INF) {
-                    sum = Math.max(sum, relation[i][j]);
-                }
+                if (i == j) continue;
+                sum = Math.max(sum, relation[i][j]);
             }
 
-            scoreArr[i] = sum;
-            candidateScore = Math.min(candidateScore, sum);
-        }
-
-        StringBuilder title = new StringBuilder();
-        StringBuilder body = new StringBuilder();
-
-        for (int i = 1; i < n+1; i++) {
-            if (candidateScore == scoreArr[i]) {
-                candidateCount++;
-                body.append(i + " ");
+            if (candidateScore > sum) {
+                candidateScore = sum;
+                candidate.clear();
+                candidate.add(i);
+            }
+            else if (candidateScore == sum) {
+                candidate.add(i);
             }
         }
 
-        title.append(candidateScore + " " + candidateCount);
+        candidateCount = candidate.size();
 
-        System.out.println(title.toString());
-        System.out.println(body.toString());
+        StringBuilder sb = new StringBuilder();
+        sb.append(candidateScore).append(" ").append(candidateCount).append("\n");
+        for (int i = 0; i < candidate.size(); i++) {
+            sb.append(candidate.get(i)).append(" ");
+        }
+
+        System.out.println(sb.toString());
     }
 }
