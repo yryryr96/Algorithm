f3362e9c7f78290d2650cac8a7712431e8a91b0d
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2024-10-11 23:21:47 +0900

    [level 3] Title: 입국심사, Time: 85.57 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,29 +1,38 @@
 import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
+        
+        // 결국 걸리는 시간의 최소값 구하기
+        // 각 상담원이 총 소요 시간동안 심사하는 인원의 총 합이 n명이면 됨
+        
         Arrays.sort(times);
-        long maxTime = (long) n * (long) times[times.length-1];
-        long minTime = (long) 1;
-        long answer = maxTime;
-
+        
+        Long maxTime = (long) n * times[times.length-1];
+        Long minTime = (long) times[0];
+        Long answer = maxTime;
+        
         while (minTime <= maxTime) {
-
-            long mid = (maxTime + minTime) / 2;
-            long temp = 0;
-
-            for (int i = 0; i < times.length; i++) {
-                temp += mid / times[i];
-                if (temp >= n) break;
+            
+            Long mid = (maxTime + minTime) / 2;
+            
+            long cnt = 0L;
+            
+            for(int i = 0; i < times.length; i++) {
+                cnt += (mid / times[i]);
+                if (cnt > n) break;
             }
-
-            if (temp < n) {
-                minTime = mid + 1;
-            } else {
-                answer = Math.min(answer, mid);
+            // System.out.println("cnt = " + cnt + " time = " + mid);
+            // 심사한 사람이 총 인원을 넘으면 시간을 더 적게잡아야함
+            if(cnt >= n) {
                 maxTime = mid - 1;
+                // 총 인원일때도 -1씩 계속 해서 탐색하므로 최소값을 저장해줘야함 -> 결국 n일때까지 탐색
+                answer = Math.min(answer, mid);
+            } else {
+            // 심사한 사람이 총 인원보다 적다면 시간 범위를 더 늘려야함
+            
+                minTime = mid + 1;
             }
         }
-
         return answer;
     }
 }
