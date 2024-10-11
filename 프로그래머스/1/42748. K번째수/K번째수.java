ecefdd3ccbc1efd1f0b48c259bd384e8abf9c573
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2024-10-11 21:02:03 +0900

    [level 1] Title: K번째수, Time: 0.37 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,18 +1,27 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-
-        int[] answer = new int[commands.length];
-        for (int i=0; i<commands.length; i++) {
+        
+        int size = commands.length;
+        int[] answer = new int[size];
+        
+        for(int i = 0; i < size; i++) {
+            
             int[] command = commands[i];
-            int start = command[0];
-            int end = command[1];
-            int idx = command[2];
+            int start = command[0] - 1;
+            int end = command[1] - 1;
+            int target = command[2] - 1;
+            
+            List<Integer> arr = new ArrayList<>();
+            for(int k = start; k <= end; k++) {
+                arr.add(array[k]);
+            }
+            
+            Collections.sort(arr);
+            answer[i] = arr.get(target);
 
-            int[] temp = Arrays.copyOfRange(array, start-1, end);
-            Arrays.sort(temp);
-            answer[i] = temp[idx-1];
         }
+        
         return answer;
     }
 }
