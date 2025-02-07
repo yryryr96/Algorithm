60fc5c828581c6731d42e0724c718fff6377617e
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2025-02-08 02:03:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.76 ms, Memory: 56.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,57 +1,64 @@
 import java.util.*;
-class Solution {
 
+class Solution {
+    
     static int n,m;
-    static int answer = -1;
-    static int[][] visited;
-    static int[] dy = {0,0,1,-1};
-    static int[] dx = {1,-1,0,0};
-    
-    public int solution(int[][] maps) {
-        
-        n = maps.length;
-        m = maps[0].length;
-        visited = new int[n][m];    
-          
-        bfs(maps);
-        return answer;
-    }
-    
-    static void bfs(int[][] maps) {
-        
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(0,0));
-        visited[0][0] = 1;
-        
-        while(!q.isEmpty()) {
-            
-            Pair cur = q.poll();
-            if(cur.y == n-1 && cur.x == m-1) {
-                answer = visited[cur.y][cur.x];
-                return;
-            }
-            
-            for(int k = 0; k < 4; k++) {
-                int ny = cur.y + dy[k];
-                int nx = cur.x + dx[k];
-                if(isRange(ny,nx) && maps[ny][nx] == 1 && visited[ny][nx] == 0) {
-                    q.add(new Pair(ny,nx));
-                    visited[ny][nx] = visited[cur.y][cur.x] + 1;
-                }
-            }
-        }
-    }
-    
-    static boolean isRange(int y, int x) {
-        return 0 <= y && y < n && 0 <= x && x < m;
-    }
-    
+    static int[][] MAP;
     static class Pair {
-        
-        int y, x;
+        int y,x;
         Pair(int y, int x) {
             this.y = y;
             this.x = x;
         }
+    }
+    static int[][] visited;
+    static int dy[] = {0,1,0,-1};
+    static int dx[] = {1,0,-1,0};
+    
+    public int solution(int[][] maps) {
+        
+        MAP = maps;
+        n = MAP.length;
+        m = MAP[0].length;
+        visited = new int[n][m];
+        
+        int answer = bfs();
+        return answer;
+    }
+    
+    static int bfs() {
+        
+        Queue<Pair> q = new LinkedList<>();
+        int sy = 0, sx = 0;
+        q.add(new Pair(sy, sx));
+        visited[sy][sx] = 1;
+        
+        int ey = n-1, ex = m-1;
+        while(!q.isEmpty()) {
+            
+            Pair cur = q.poll();
+            
+            if (cur.y == ey && cur.x == ex) {
+                return visited[ey][ex];
+            }
+            
+            for (int d = 0; d < 4; d++) {
+                int ny = cur.y + dy[d];
+                int nx = cur.x + dx[d];
+                
+                if (!isInRange(ny,nx)) continue;
+                if (visited[ny][nx] != 0) continue;
+                if (MAP[ny][nx] == 0) continue;
+                
+                q.add(new Pair(ny,nx));
+                visited[ny][nx] = visited[cur.y][cur.x] + 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    static boolean isInRange(int y, int x) {
+        return 0 <= y && y < n && 0 <= x && x < m;
     }
 }
