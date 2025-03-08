3eccee59f139798ce44ff782028978f6e4fdd711
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2025-03-08 23:13:36 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 576 ms, Memory: 28324 KB -BaekjoonHub

--- 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
@@ -5,7 +5,6 @@ public class Main {
 
     static int N, M, R;
     static int[][] board;
-    static int[][][] dp;
     static int[] dy = {0, 1, 0, -1};
     static int[] dx = {1, 0, -1, 0};
 
@@ -18,7 +17,6 @@ public class Main {
         M = stoi(st.nextToken());
         R = stoi(st.nextToken());
         board = new int[N][M];
-        dp = new int[(N+M)*2 - 3][N][M];
 
         for (int row = 0; row < N; row++) {
             st = new StringTokenizer(br.readLine());
@@ -27,15 +25,10 @@ public class Main {
             }
         }
 
-        for (int i = 1; i <= R; i++) {
-            if((N+M)*2 - 3 < i) break;
-            rotate(i);
-        }
-
         int depthLimit = Math.min(N,M)/2 - 1;
         for (int depth = 0; depth <= depthLimit; depth++) {
-            int p = ((N - depth*2) + (M - depth*2)) * 2 - 4;
-            refreshBoard(depth, R%p);
+            int cnt = R % (((N - depth*2) + (M - depth*2)) * 2 - 4);
+            rotate(depth, cnt);
         }
 
         StringBuilder sb = new StringBuilder();
@@ -49,50 +42,13 @@ public class Main {
         System.out.println(sb.toString());
     }
 
-    static void rotate(int r) {
-        int depthLimit = Math.min(N,M)/2 - 1;
-        for (int depth = 0; depth <= depthLimit; depth++) {
-            int p = ((N - depth*2) + (M - depth*2)) * 2 - 4;
-//            System.out.println("r%p = " + r%p);
-//            System.out.println("r = " + r);
-//            System.out.println("p = " + p);
-            if (dp[r%p][depth][depth] == 0) {
-                // dp 값 활용
-                initDp(depth, r%p);
-                refreshBoard(depth, r%p);
-            } 
+    static void rotate(int depth, int cnt) {
+        for (int c = 0; c < cnt; c++) {
+            rotateLayer(depth);
         }
     }
 
-    static void refreshBoard(int depth, int v) {
-        board[depth][depth] = dp[v][depth][depth];
-        int cy = depth;
-        int cx = depth;
-
-        int d = 0;
-        while (true) {
-
-            int ny = cy + dy[d];
-            int nx = cx + dx[d];
-            if (ny < depth || nx < depth || ny >= N-depth || nx >= M-depth) {
-                d++;
-                ny = cy + dy[d];
-                nx = cx + dx[d];
-            }
-
-            if (ny == depth && nx == depth) {
-                break;
-            }
-
-            board[ny][nx] = dp[v][ny][nx];
-//            System.out.println("v = " + v);
-//            System.out.println("dp[v][ny][nx] = " + dp[v][ny][nx]);
-            cy = ny;
-            cx = nx;
-        }
-    }
-
-    static void initDp(int depth, int v) {
+    static void rotateLayer(int depth) {
 
         int temp = board[depth][depth];
         int cy = depth;
@@ -109,11 +65,11 @@ public class Main {
             }
 
             if (ny == depth && nx == depth) {
-                dp[v][cy][cx] = temp;
+                board[cy][cx] = temp;
                 break;
             }
 
-            dp[v][cy][cx] = board[ny][nx];
+            board[cy][cx] = board[ny][nx];
             cy = ny;
             cx = nx;
         }
