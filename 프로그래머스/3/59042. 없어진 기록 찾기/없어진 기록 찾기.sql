45346690deb2aa3063960ebcd59719b4f3823371
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2025-05-16 01:34:32 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,7 @@
 -- 코드를 입력하세요
-# SELECT AO.ANIMAL_ID, AO.NAME FROM ANIMAL_OUTS as AO LEFT JOIN ANIMAL_INS as AI ON AO.ANIMAL_ID = AI.ANIMAL_ID WHERE AI.ANIMAL_ID IS NULL ORDER BY AO.ANIMAL_ID;
-
-select ao.animal_id, ao.name from animal_outs ao where ao.animal_id not in (select ai.animal_id from animal_ins ai) order by ao.animal_id;
-
+SELECT AO.ANIMAL_ID, AO.NAME 
+FROM ANIMAL_OUTS AS AO
+LEFT OUTER JOIN ANIMAL_INS AS AI
+ON AO.ANIMAL_ID = AI.ANIMAL_ID
+WHERE AI.ANIMAL_ID IS NULL
+ORDER BY AO.ANIMAL_ID;
