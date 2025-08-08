c2b67677fefb766783b7854b717d1c9387367285
YoungRok <s9285s@naver.com>
2025-08-09 01:02:15 +0900

    [level 2] Title: 타겟 넘버, Time: 5.88 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,30 +1,29 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
     
     static int answer = 0;
-    static int T;
+    static int N;
+    
     public int solution(int[] numbers, int target) {
         
-        T = target;
-        dfs(0, 0, 0, numbers);
+        N = numbers.length;
+        dfs(0, 0, numbers, target);
         
         return answer;
     }
     
-    static void dfs(int depth, int idx, int value, int[] numbers) {
+    static void dfs(int depth, int value, int[] numbers, int target) {
         
-        if (depth == numbers.length) {
-            
-            if (value == T) {
+        if (depth == N) {
+            if (value == target) {
                 answer++;
             }
             return;
         }
-        
-        for(int i = idx; i < numbers.length; i++) {
-            dfs(depth+1, i+1, value + numbers[i], numbers);
-            dfs(depth+1, i+1, value - numbers[i], numbers);
-        }
+       
+        dfs(depth + 1, value + numbers[depth], numbers, target);
+        dfs(depth + 1, value - numbers[depth], numbers, target);
     }
 }
